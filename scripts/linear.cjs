5083f1f812043ba080e92bd4a1863125be0e95b0
Ivan Kuznetsov <kuznetsovivan496@gmail.com>
2026-01-30 16:23:06 +0200

    Cleanup: consolidate linear scripts to scripts/linear.mjs

--- scripts/linear.cjs
+++
@@ -1,8 +0,0 @@
-// CommonJS wrapper around the Linear helper.
-// This exists so automation can `require('./scripts/linear.cjs')` even though
-// the repo is ESM (`type: module`).
-//
-// Prefer the ESM entrypoint for human use:
-//   node scripts/linear.js ...
-
-module.exports = require('./linear-helper.cjs');
